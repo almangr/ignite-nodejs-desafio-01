7562b3e65600cce7447f28b810442217bc1ad616
Antonio Luiz Mangueira <developer@mangueira.net>
2021-12-30 15:29:22 -0300

    feat: add verification if user account exists

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4 } = require('uuid');
+const
+  { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -11,7 +12,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find((user) => user.username === username)
+
+  if (!user) {
+    return response.status(400).json({ error: "User not found!" })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
